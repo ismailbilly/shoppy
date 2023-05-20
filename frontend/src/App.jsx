51afe9237192955c9523838aae8289b564f5f159
Bilqisu Ismail <ismail.bilkisu@gmail.com>
2023-05-20 14:23:27 +0100

    project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import {BrowserRouter, Routes,Route} from 'react-router-dom'
+import {LoginPage} from './Routes.js'
+function App() {
+  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<LoginPage/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
